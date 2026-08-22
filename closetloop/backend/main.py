514e756c6988c256bea312e1e0859ed155785de2
67160385-buu <67160385@go.buu.ac.th>
2026-08-22 11:09:58 +0700

    Delete closetloop directory

--- closetloop/backend/main.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-
-app = FastAPI()
-
-# เมื่อมีคนเรียกมาที่หน้าแรก ให้ส่งข้อความทักทายกลับไป
-@app.get("/")
-def read_root():
-    return {"message": "สวัสดีครับ! ห้องครัวหลังบ้าน ClosetLoop พร้อมทำงานแล้ว"}
